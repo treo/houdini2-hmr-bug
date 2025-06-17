8425b75bfcbaa5d4969b2ee5d0c620f2ee211a1b
Paul Dubs <paul.dubs@gmail.com>
2025-06-17 15:36:14 +0200

    Missing files

---
+++ houdini.config.js
@@ -0,0 +1,12 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+    "runtimeDir": ".houdini",
+    "schemaPath": "schema.graphql",
+    "plugins": {
+        "houdini-svelte": {}
+    }
+}
+
+export default config
